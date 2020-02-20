9dfca9d3f6740f46d8cd21f091faf4c50b44e7e9
Mike Ludemann <mike-ludemann@hotmail.de>
2020-02-20 14:31:24 +0100

    Setup complete - vue-textarea

---
+++ src/components/textarea.vue
@@ -0,0 +1,52 @@
+<template lang="html">
+
+	<div class="textarea">
+		<textarea :id="id" :rows="row" :cols="column" class="form" :name="name" @input="changeHandling" @change="changeHandling" />
+	</div>
+
+</template>
+
+<script lang="js">
+
+	export default {
+		name: 'customTextarea',
+		props: {
+			id: String,
+			name: String,
+			row: String,
+			column: String
+		},
+		mounted () {
+
+		},
+		data () {
+			return {
+
+			}
+		},
+		methods: {
+			changeHandling ({ type, target }) {
+			const event = {
+				type,
+				isCheckbox: target.type === 'checkbox',
+				target: {
+					value: target.value,
+					checked: target.checked
+				}
+			}
+			console.log(`${event.type}: ${event.isCheckbox ? event.target.checked : event.target.value}`);
+			}
+		},
+		computed: {
+
+		}
+}
+
+
+</script>
+
+<style scoped lang="scss">
+	.textarea {
+
+	}
+</style>
